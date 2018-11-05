da5c8ab602234472e867941020b0615187575889
Koffi Anderson Koffi <kandersonko@gmail.com>
2018-11-05 12:15:55 -0800

    add Grammar class

--- grammar.py
+++ grammar.py
@@ -1,12 +1,15 @@
 class Grammar(object):
 
-    def __init__(G):
+    def __init__(self, G):
         """ __init__ takes a string G
             and parses parses the productions into an array of productions 
         """
-        pass
-    
-    def getProductions():
-        """  getProductions returns the array of productions
-        """
-        pass
+        self.rules = G.split(';');
+        self.productions = dict()
+        for rule in self.rules:
+            startVar = rule.split('->')[0]
+            varSet = rule.split("->")[1]
+            variables = [x for x in varSet.split('|') if x.isupper()]
+            terminals = [x for x in varSet.split('|') if x.islower()]
+            self.productions[startVar] = {"variables": variables, "terminals": terminals}
+
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from grammar import Grammar
+
+
 def main():
-    G = Grammar("s->AB|a;A->BB|a;B->AB|b")
-    cykAlgo = CYKAlgo(G)
-    if(cykAlgo.membership(w)){
-        print "w is in G"
-    } else {
-        print "w is not in G"
-    }
+    G = Grammar("S->AB|a;A->BB|a;B->AB|C|b;C->c;D->D")
+    print(G.rules)
+    print(G.productions)
+    
+    # cykAlgo = CYKAlgo(G)
+    # if(cykAlgo.membership(w)){
+    #     print "w is in G"
+    # } else {
+    #     print "w is not in G"
+    # }
 
 if __name__ == '__main__':
     main()
